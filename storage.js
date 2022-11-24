bb4b31205c7d8215e0503cc3b5b500b18ac1b795
alexrostkowska <aleksandra.rostkowskaa@gmail.com>
2022-11-24 11:16:46 +0100

    Projekt1-addingTableLayout

--- storage.js
+++ storage.js
@@ -1,9 +1,9 @@
 class Product{
     constructor(name, quantity, price){
-        this.name = name
-        this.quantity= quantity
-        this.price = price
-        this.sum = this.price*this.quantity
+        this.name = name;
+        this.quantity= quantity;
+        this.price = price;
+        this.sum = this.price*this.quantity;
     }
    
 }
@@ -14,63 +14,68 @@ function getdate(){
 }
 
 function deleteProductFromLS(i){
-    localStorage.removeItem(i)
-    console.log(localStorage)
+    localStorage.removeItem(i);
+    console.log(localStorage);
 }
  function addProductToLS(p,i){
-        var pr = {
+        const pr = {
             'nazwa': p.name,
             'cena': p.price,
             'ilość':p.quantity,
             'suma':p.sum
         }
-        localStorage.setItem(i,JSON.stringify(pr))
+        localStorage.setItem(i,JSON.stringify(pr));
 
     }
 function getProduct(id){
-    let pr = JSON.parse(localStorage[id])
-    return pr
+    let pr = JSON.parse(localStorage[id]);
+    return pr;
 
 }
 
 function tableCreate() {
-    const body = document.body,
+    const body = document.body;
     tbl = document.createElement('table');
-    tbl.style.width = '100px';
-    tbl.style.border = '1px solid black';
+    tbl.style.width = "auto";
+    tbl.style.border = "1px solid black";
     
     //add headres
-    var titles = ["nazwa","cena","ilość",'suma']
-    const header = tbl.createTHead();
-    var row = header.insertRow()
-    var cell = row.insertCell()
-    cell.style.border = '1px solid black';
+    const titles = ["nazwa","cena","ilość","suma"];
+    let header = tbl.createTHead();
+    let row = header.insertRow();
+    let cell = row.insertCell();
+    cell.style.border = "1px solid black";
     //add content
     for (let i=1;i<titles.length+1;i++){
-        var cell = row.insertCell(i)
-        cell.innerHTML = titles[i-1]
-        cell.style.border = '1px solid black';
+        let cell = row.insertCell(i);
+        cell.innerHTML = titles[i-1];
+        cell.style.border = "1px solid black";
     }
     for(let i =0;i<localStorage.length;i++){
-        const tr = tbl.insertRow();
-        tr.value = i
-        var item = getProduct(i+1)
+        let tr = tbl.insertRow();
+        tr.value = i;
+        const item = getProduct(i+1);
         for(let j =0;j<titles.length+2;j++){
-            const td = tr.insertCell()
+            let td = tr.insertCell();
             //Delete button
             if(j==5){
-                td.insert
-                var svgPath = 'bin.svg';
+                td.insert;
+                //const svgPath = "bin.svg";
 
-                var btn = document.createElement('input')
-                btn.type = 'button'
-                btn.className = 'btn'
-                let img = document.createElement('svg')
-                img.src = svgPath
-                btn.value = img
-
-                btn.onclick = function(){deleteRow(tr.value)}
-                td.appendChild(btn)
+                const btn = document.createElement('input');
+                btn.type = "button";
+                btn.className = "btn";
+                //const img = document.createElement('img');
+                //img.src = "bin.svg";
+                //img.src = "bin.svg";
+                btn.style.backgroundImage = "url('delete.png')";
+                btn.style.backgroundSize = "contain";
+                btn.style.backgroundRepeat = "no-repeat";
+                
+                btn.onclick = function(){
+                    deleteRow(tr.value);
+                };
+                td.appendChild(btn);
                 
 
 
@@ -81,18 +86,20 @@ function tableCreate() {
 
             }
             //print items
-            else{td.appendChild(document.createTextNode(item[titles[j-1]]));}
+            else if(j != 5){
+                td.appendChild(document.createTextNode(item[titles[j-1]]));
+            }
             td.style.border = '1px solid black';
 
 
         }
     }
     function deleteRow(id){
-        var rows = tbl.rows
+        var rows = tbl.rows;
         for (let index = 0; index < rows.length; index++) {
             if(rows[index].value == id){
-                tbl.deleteRow(index)
-                deleteProductFromLS(index)
+                tbl.deleteRow(index);
+                deleteProductFromLS(index);
             }
 
             
@@ -102,17 +109,20 @@ function tableCreate() {
 
     body.appendChild(tbl);
 }
-  
-let pr = new Product('Jabłka',1.5,4.9)
-addProductToLS(pr,1)
-pr = new Product('Bułka',5,0.49)
-addProductToLS(pr,2)
-pr = new Product('Pomidor',2,1.59)
-addProductToLS(pr,3)
+ 
+let pr = new Product('Jabłka',1.5,4.9);
+addProductToLS(pr,1);
+pr = new Product('Bułka',5,0.49);
+addProductToLS(pr,2);
+pr = new Product('Pomidor',2,1.59);
+addProductToLS(pr,3);
 // write current date to title
-txt = document.createTextNode('Paragon '+getdate());
-t = document.getElementsByTagName('h1')
-t.innerText = t.textContent = txt;
+//txt = document.createTextNode('Paragon '+getdate());
+const date = new Date();
+
+const t = document.getElementsByTagName("h1");
+//let data = getdate();
+document.getElementsByTagName("h1").innerHTML = "Paragon " + date.toLocaleDateString("pl");
 
 
-tableCreate()
+tableCreate();
